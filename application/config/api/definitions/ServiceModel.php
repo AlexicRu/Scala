580981fef76a7eb877b5e2c6af3a6473a89d2df5
paShaman <nikitin696@gmail.com>
2018-02-09 16:14:55 +0300

    2018-02-09 #512

--- application/config/api/definitions/ServiceModel.php
+++ application/config/api/definitions/ServiceModel.php
@@ -3,26 +3,11 @@
 return [
     'type' => 'object',
     'properties' => [
-        'contract_id' => [
+        'service_id' => [
             'type' => 'integer'
         ],
-        'contract_name' => [
+        'system_service_name' => [
             'type' => 'string'
-        ],
-        'date_begin' => [
-            'type' => 'string',
-            'description' => 'd.m.Y'
-        ],
-        'date_end' => [
-            'type' => 'string',
-            'description' => 'd.m.Y'
-        ],
-        'currency' => [
-            'type' => 'integer',
-            'default' => Common::CURRENCY_RUR
-        ],
-        'contract_status' => [
-            'type' => 'integer'
         ]
     ]
 ];
