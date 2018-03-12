0ad3a4428a0794d063fa23c803bd50aab5067c9a
paShaman <nikitin696@gmail.com>
2018-03-12 16:13:00 +0300

    2018-03-12 add instruction

--- application/views/pages/support/index.php
+++ application/views/pages/support/index.php
@@ -2,11 +2,15 @@
 
 <div class="tabs_block tabs_switcher">
     <div class="tabs">
-        <span tab="feedback" class="tab active"><i class="icon-notifications"></i> Обратная связь</span>
+        <span tab="feedback" class="tab active"><i class="icon-notifications"></i> Обратная связь</span><span tab="documents" class="tab"><i class="icon-contract"></i> Документы</span>
     </div>
-    <div class="tabs_content tabs_content_no_padding">
+    <div class="tabs_content">
         <div tab_content="feedback" class="tab_content active">
             <?=$feedbackForm?>
+        </div>
+        <div tab_content="documents" class="tab_content">
+            <b>Инструкции:</b><br><br>
+            <span class="f20">Инструкция по работе с ЛК системы</span> &nbsp; <a href="/upload/files/Инструкция_по_работе_с_ЛК_системы.docx" class="btn btn_small"><i class="icon-download icon"></i> Скачать</a>
         </div>
     </div>
 </div>
