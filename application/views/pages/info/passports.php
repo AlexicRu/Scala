c0c0d33e38a5e0414db470faa25fbfcce87c0e3f
paShaman <nikitin696@gmail.com>
2018-06-28 12:47:24 +0300

    2018-06-28 passports

--- application/views/pages/info/passports.php
+++ application/views/pages/info/passports.php
@@ -1,1 +1,60 @@
 <h1>Паспорта качества</h1>
+
+<div class="as_table as_table__dashboard">
+    <div class="col" style="width: 33%">
+        <div class="block">
+            <h2>Паспорта качества</h2>
+
+            <div class="info__list">
+                <a href="/file/info/Паспорта_качества/92.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-file"></span></div>
+                        <div class="info__item-name">92</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/95.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">95</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/95G.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">95G</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Декларация_ДТС.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Декларация_ДТС</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/ДТФ.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-file"></span></div>
+                        <div class="info__item-name">ДТФ</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Опти.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Опти</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="col" style="width: 33%">
+
+    </div>
+    <div class="col" style="width: 33%">
+
+    </div>
+</div>
