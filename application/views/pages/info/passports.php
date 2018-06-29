d6d6784fb584e99ff712484cd5f00f984df0def1
paShaman <nikitin696@gmail.com>
2018-06-29 10:58:51 +0300

    2018-06-29

--- application/views/pages/info/passports.php
+++ application/views/pages/info/passports.php
@@ -48,13 +48,149 @@
                     </div>
                     <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
                 </a>
+                <a href="/file/info/Паспорта_качества/Кемерово.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Кемерово</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
             </div>
         </div>
     </div>
     <div class="col" style="width: 33%">
+        <div class="block">
+            <h2>Сибирь</h2>
 
+            <div class="info__list">
+                <a href="/file/info/Паспорта_качества/Сибирь/95_Опти.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">95 Опти</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/ДТзОпти_5_518_от_22.03.2018-вр.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">ДТзОпти 5 518 от 22.03.2018-вр</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/Паспорт_92_23_01_2018.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Паспорт 92 23.01.2018</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/Паспорт_92_Опти.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Паспорт 92 Опти</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/Паспорт_95_23_01_2018.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Паспорт 95 23.01.2018</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/Паспорт_ДТЗ_21_01_2018.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Паспорт ДТЗ 21.01.2018</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/Паспорт_ДТЗ_ЕВРО.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Паспорт ДТЗ ЕВРО</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+            </div>
+        </div>
     </div>
     <div class="col" style="width: 33%">
+        <div class="block">
+            <h2>Урал</h2>
 
+            <div class="info__list">
+                <a href="/file/info/Паспорта_качества/Сибирь/АИ-95-К5_ГОСТ_32513-2013.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">АИ-95-К5 ГОСТ 32513-2013</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/Газ_углеводородный_сжиженный_ГОСТ_Р_52087-2003.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Газ углеводородный сжиженный ГОСТ Р 52087-2003</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/Газ_углеводородный_сжиженный_топливный_для_коммунально-бытового_потребления_марки_ПТ_пропан технический_по_ГОСТ_20448-90_50295605_v1.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Газ углеводородный сжиженный топливный для коммунально-бытового потребления марки ПТ (пропан технический) по ГОСТ 20448-90 (50295605&nbsp;v1)</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/Дизель_Опти_ДТ-З-К5.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Дизель Опти ДТ-З-К5</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/ДТ_арктическое_ДТ-А-К5_минус_44.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">ДТ арктическое ДТ-А-К5 минус 44</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/ДТ_ЕВРО_К5_ГОСТ_32511-2013.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">ДТ ЕВРО К5 ГОСТ 32511-2013</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/ДТ_ЕВРО_К5_ГОСТ_Р_52368-2005.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">ДТ ЕВРО К5 ГОСТ Р 52368-2005</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/Премиум_Евро-95_К5.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Премиум Евро-95 К5</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/Регуляр-92_К5.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Регуляр-92 К5</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+                <a href="/file/info/Паспорта_качества/Сибирь/Регуляр-92_ГОСТ_32513-2013.pdf" class="info__item" target="_blank">
+                    <div>
+                        <div><span class="info__icon info__icon-pdf"></span></div>
+                        <div class="info__item-name">Регуляр-92 ГОСТ 32513-2013</div>
+                    </div>
+                    <span class="btn btn_icon btn_reverse info__item-btn"><i class="icon-download"></i></span>
+                </a>
+            </div>
+        </div>
     </div>
 </div>
