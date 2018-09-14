2981a36b39cf0278feded065ffc56e03db89edcd
paShaman <nikitin696@gmail.com>
2018-09-14 12:15:10 +0300

    2018-09-14

---
+++ application/classes/Controller/Sms.php
@@ -0,0 +1,67 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Sms extends Controller_Common
+{
+    /**
+     * аяксово грузим поставщиков
+     */
+    public function action_sendConfirmCode()
+    {
+        $phone = $this->request->post('phone');
+
+        /**
+         * @var Sender_Sms $sender
+         */
+        $sender = Sender::factory('sms');
+
+        $result = $sender->sendConfirmCode($phone);
+
+        if (empty($result)) {
+            $this->jsonResult(false, 'Не удалось отправить код');
+        }
+
+        $this->jsonResult(true, $result);
+    }
+
+    /**
+     * подключаем смс информирование
+     */
+    public function action_enableSmsInform()
+    {
+        $phone = $this->request->post('phone');
+        $code = $this->request->post('code');
+
+        /**
+         * @var Sender_Sms $sender
+         */
+        $sender = Sender::factory('sms');
+
+        if (!$sender->checkConfirmCode($phone, $code)) {
+            $this->jsonResult(false, "Неверный код");
+        }
+
+        $manager = Model_Manager::getManager(['PHONE_FOR_SMS' => $phone]);
+
+        //$result = Model_Manager::disableSmsInform($manager['MANAGER_ID']); //todo
+
+        if (!empty($result)) {
+            $this->jsonResult(true);
+        }
+        $this->jsonResult(false);
+    }
+
+    /**
+     * отключаем смс информирование
+     */
+    public function action_disableSmsInform()
+    {
+        $user = User::current();
+
+        //$result = Model_Manager::disableSmsInform($user['MANAGER_ID']); //todo
+
+        if (!empty($result)) {
+            $this->jsonResult(true);
+        }
+        $this->jsonResult(false);
+    }
+}
