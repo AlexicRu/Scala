d299565f0d72975bfa1d025bac076d32a4de73f7
paShaman <nikitin696@gmail.com>
2018-11-12 12:36:55 +0300

    2018-11-12

--- scripts/shell_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$database = [
-    'db'        => '(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 10.8.2.1)(PORT = 1521)))(CONNECT_DATA=(SID=KF)(SERVER=DEDICATED)))',
-    'name'      => 's_web',
-    'password'  => '61a5yh1jeq',
-];
-
-$shellUrl = 'https://api-emea.prod.emea.wexinc.co.uk';
